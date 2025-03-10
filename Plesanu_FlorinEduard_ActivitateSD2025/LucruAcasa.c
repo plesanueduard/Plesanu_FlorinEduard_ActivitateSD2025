e47bd469db0b1c4d5bd302a43fbed139842db675
Plesanu Eduard <redlol1@yahoo.com>
2025-03-10 16:41:17 +0200

    Functie modifica pret+afisarea acesteia

--- Plesanu_FlorinEduard_ActivitateSD2025/LucruAcasa.c
+++ Plesanu_FlorinEduard_ActivitateSD2025/LucruAcasa.c
@@ -24,17 +24,20 @@ void afisare(struct Telefon t) {
 	printf("%d Telefonul %s seria %c are ram: %d si costa %5.2f RON\n",t.id,t.producator,t.serie,t.ram,t.pret); //fiecare % e pt afisare de fiecare variabila
 }
 
-void modifica_Atribut(struct Telefon t) {
-	
+void modificaPret(struct Telefon* t,float pretNou) {
+	if (pretNou > 0) {  
+		t->pret = pretNou;  // daca nu foloseam pointer ne ramanea acelasi pret, practic trimitem din main prin adresa o valoare in functia asta
+	}
 }
 
 void dezalocare(struct Telefon* t) {
-	free(t);
 }
 
 int main() {
 	struct Telefon t;
 	t = initializare(1, 16, "Samsung",1600.25,'A');   // la char se pune '' iar la char* se pune"" adica un fel de string e char*
+	afisare(t);
+	modificaPret(&t, 1000); // trebuie folosit & ca sa preia valoarea in functie
 	afisare(t);
 	return 0;
 }
