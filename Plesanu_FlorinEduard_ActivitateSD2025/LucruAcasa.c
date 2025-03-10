35522d6a9444ac76cc6fa323022237c66657f7d0
Plesanu Eduard <redlol1@yahoo.com>
2025-03-10 16:31:57 +0200

    Initializare si afisare

--- Plesanu_FlorinEduard_ActivitateSD2025/LucruAcasa.c
+++ Plesanu_FlorinEduard_ActivitateSD2025/LucruAcasa.c
@@ -1,35 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct Sablon {
+struct Telefon {
 	int id;
-	//int ;
-	//char* ;
-	//float ;
-	//char ;
+	int ram;
+	char* producator;
+	float pret;
+	char serie;
 };
 
-struct Sablon initializare(/*parametri necesari*/) {
-	struct Sablon s;
-	s.id = 1;
-	//initializare structura 
-	return s;
+struct Telefon initializare(int id,int ram,const char* producator,float pret,char serie) {
+	struct Telefon t;
+	t.id = id;
+	t.ram = ram;
+	t.producator = (char*)malloc(sizeof(char)*(strlen(producator)+1));  //creeam loc pentru ce introducem de la tastatura
+	strcpy_s(t.producator,strlen(producator)+1,producator);
+	t.pret = pret;
+	t.serie = serie;
+	return t;
 }
 
-void afisare(struct Sablon s) {
-	//afisarea tuturor atributelor.
+void afisare(struct Telefon t) {
+	printf("%d Telefonul %s seria %c are ram: %d si costa %5.2f RON\n",t.id,t.producator,t.serie,t.ram,t.pret); //fiecare % e pt afisare de fiecare variabila
 }
 
-void modifica_Atribut(struct Sablon s) {
-	//modificarea unui atribut
+void modifica_Atribut(struct Telefon t) {
+	
 }
 
-void dezalocare(struct sablon* s) {
-	//dezalocare campuri alocate dinamic
+void dezalocare(struct Telefon* t) {
+	free(t);
 }
 
 int main() {
-	struct Sablon s;
-
+	struct Telefon t;
+	t = initializare(1, 16, "Samsung",1600.25,'A');   // la char se pune '' iar la char* se pune"" adica un fel de string e char*
+	afisare(t);
 	return 0;
 }
